d31d30feb3874055f7715411a5ebe98c2d95c4d2
edwardfgthomas@gmail.com <edwardfgthomas@gmail.com>
2023-01-31 14:15:15 +0000

    create find method in album_repository

--- spec/album_repository_spec.rb
+++ spec/album_repository_spec.rb
@@ -11,12 +11,18 @@ describe AlbumRepository do
     reset_albums_table
   end
 
-
   it "returns the list of albums" do
     repo = AlbumRepository.new
     albums = repo.all
-
     expect(albums.length).to eq 2
     expect(albums.first.title).to eq "Bossanova"
+  end
+
+  it "returns a single album object based on criteria " do
+    repo = AlbumRepository.new
+    albums = repo.find(1)
+    expect(albums.title).to eq "Bossanova"
+    albums = repo.find(2)
+    expect(albums.release_year).to eq "2002"
   end
 end
